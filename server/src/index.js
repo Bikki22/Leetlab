36680918e70e8975dadbc304733bcbe7664c4fca
Bikki chaudhary <cbikki35@gmail.com>
2025-11-18 03:04:38 +0545

    complete authentication

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
+dotenv.config();
+
 const PORT = process.env.PORT ?? 8080;
+
+app.use(express.json());
+app.use(
+  cors({
+    // origin: "process.env.BASE_URI",
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running in http://localhost:${PORT}`);
